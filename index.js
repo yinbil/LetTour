5a2b3b4f3cab9e28f9f3a1138b6b8d4704d39eca
Francis Yinbil <yinbilfrancis@yahoo.com>
2016-10-14 17:32:34 +0000

    Third commit

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+	//Camera Here
 (function(){
 	document.addEventListener('deviceready', onDeviceReady.bind(this), false);
 	var pictureSource ;
@@ -27,4 +28,43 @@
 	}
 
 })();
+
+	//Geolocation Here
+(function(){
+	var x = document.getElementById("location");
+
+	function getLocation() {
+	    if (navigator.geolocation) {
+	        navigator.geolocation.getCurrentPosition(showPosition);
+	    } else {
+	        x.innerHTML = "Geolocation is not supported by this browser.";
+	    }
+	}
+
+	function showPosition(position) {
+	    x.innerHTML = "Latitude: " + position.coords.latitude +
+	    "<br>Longitude: " + position.coords.longitude;
+	}
+})();
+
+	// Barcode Scanner Here
+
+cordova.plugins.barcodeScanner.scan(
+      function (result) {
+          alert("We got a barcode\n" +
+                "Result: " + result.text + "\n" +
+                "Format: " + result.format + "\n" +
+                "Cancelled: " + result.cancelled);
+      },
+      function (error) {
+          alert("Scanning failed: " + error);
+      },
+      {
+          "preferFrontCamera" : true, // iOS and Android
+          "showFlipCameraButton" : true, // iOS and Android
+          "prompt" : "Place a barcode inside the scan area", // supported on Android only
+          "formats" : "QR_CODE,PDF_417", // default: all but PDF_417 and RSS_EXPANDED
+          "orientation" : "landscape" // Android only (portrait|landscape), default unset so it rotates with the device
+      }
+   );
 
